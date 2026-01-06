107b8747ce0282f69d5b8ef3e6b75819ec964fbc
mafe <mfernandarocosta@gmail.com>
2026-01-06 00:22:25 -0300

    feat: registra modelos de Patrimonio, Categoria e Localizacao no admin

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Categoria, Localizacao, Patrimonio
 
 # Register your models here.
+@admin.register(Patrimonio)
+class PatrimonioAdmin(admin.ModelAdmin):
+    list_display = ('numero_tombamento', 'nome', 'categoria', 'localizacao', 'estado')
+    search_fields = ('nome', 'numero_tombamento')
+    list_filter = ('estado', 'categoria', 'localizacao')
+
+admin.site.register(Categoria)
+admin.site.register(Localizacao)
