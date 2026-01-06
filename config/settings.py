6e8385b9f77d1a04a7fdd43ce4ec7254b624367e
mafe <mfernandarocosta@gmail.com>
2026-01-06 08:55:40 -0300

    feat: adiciona redirecionamentos de login e logout nas configurações

--- config/settings.py
+++ config/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'core',
+    'usuarios',
 ]
 
 MIDDLEWARE = [
@@ -121,3 +122,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL = 'lista_patrimonio'
+
+LOGOUT_REDIRECT_URL = 'login'
